f34fcc643020e4487171364f2d5e304edda9962d
Deepti ranjan <deeptiranjan3.drb@gmail.com>
2023-07-05 23:28:16 +0530

    Initial commit

---
+++ src/main/java/com/example/jpademo/JpademoApplication.java
@@ -0,0 +1,22 @@
+package com.example.jpademo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class JpademoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpademoApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args){
+        return runner -> {
+            System.out.println("Hello world");
+        };
+    }
+
+}
