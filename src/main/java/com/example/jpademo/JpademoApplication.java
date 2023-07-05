177035c32579ade35f2acb4fafea0339685c652c
Deepti ranjan <deeptiranjan3.drb@gmail.com>
2023-07-06 00:48:19 +0530

    added dao and entity class. Created demo student

--- src/main/java/com/example/jpademo/JpademoApplication.java
+++ src/main/java/com/example/jpademo/JpademoApplication.java
@@ -1,5 +1,7 @@
 package com.example.jpademo;
 
+import com.example.jpademo.dao.StudentDAO;
+import com.example.jpademo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,23 @@ public class JpademoApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args){
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
         return runner -> {
-            System.out.println("Hello world");
+          createStudent(studentDAO);
         };
+    }
+
+    private void createStudent(StudentDAO studentDAO){
+        // create the student object
+        System.out.println("Creating new student object ...");
+        Student tempStudent = new Student("Paul", "Doe", "paul@gmail.com");
+
+        // save the student object
+        System.out.println("Saving the student ...");
+        studentDAO.save(tempStudent);
+        // display the id of the saved student
+        System.out.println("Saved student. Generated id: " + tempStudent.getId());
+
     }
 
 }
---
+++ src/main/java/com/example/jpademo/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.example.jpademo.dao;
+
+import com.example.jpademo.entity.Student;
+
+public interface StudentDAO {
+    void save(Student student);
+}
